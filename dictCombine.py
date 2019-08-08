644555a8b779e89320eb67ef4e03ee11e508cc30
1ierro1ast <45067475+1ierro1ast@users.noreply.github.com>
2019-08-08 17:12:41 +0400

    first commit

---
+++ dictCombine.py
@@ -0,0 +1,147 @@
+import sys, subprocess, hashlib, zlib, random
+
+
+def saveToFile(data,filename):
+	with open(filename, 'a',encoding = "UTF-8") as file:
+		file.write(data+'\n')
+		file.close
+	return data
+
+
+def hasher(hashType,data):
+	if hashType == "md5":
+		res = str(hashlib.md5(bytes(data, encoding = "UTF-8")).hexdigest())
+	elif hashType == "sha1":
+		res = str(hashlib.sha1(bytes(data, encoding = "UTF-8")).hexdigest())
+	elif hashType == "sha2":
+		res = str(hashlib.sha2(bytes(data, encoding = "UTF-8")).hexdigest())
+	elif hashType == "sha256":
+		res = str(hashlib.sha256(bytes(data, encoding = "UTF-8")).hexdigest())
+	elif hashType == "sha512":
+		res = str(hashlib.sha512(bytes(data, encoding = "UTF-8")).hexdigest())
+	else:
+		return print("incorrect hash type")
+	return res
+
+
+def hashData(hashType, dataList, filename):
+	for i in dataList:
+		hashData = hasher(hashType = hashType, data = i)
+		saveToFile(data = str(hashData), filename = filename)
+	return filename
+
+
+def openDict(dictHash):
+	with open(dictHash,"r",encoding = "UTF-8") as file:
+		data = file.read()
+		dataList = data.split("\n")
+		return dataList
+
+
+def brute(dataList,search):
+	for i in dataList:
+		if i == search:
+			return i
+			break
+
+
+def bruteHash(dataList,data,hashType):
+	for i in dataList:
+		if str(hasher(hashType = hashType, data = i)) == data:
+			return i
+			break
+
+
+def genPassDict(filename,minSize,maxSize,sizeList):
+	ruA = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
+	enA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	
+	for i in range(0,sizeList):
+		lenW = random.randint(minSize,maxSize)
+		word = ""
+		for j in range(0,lenW):
+			g = random.randint(0,len(ruA)-1)
+			word += ruA[g]
+		saveToFile(word,filename)
+	return "success"
+
+def logo():
+	return splitter()+"\n|=========dictCombine==="
+
+def splitter():
+	return "\n|======================="
+
+
+def inputDataBrute():
+	print(splitter())
+	passwordBase = input("| Enter password base name: ")
+	word = input("| Enter check word: ")
+	return passwordBase, word
+
+def inputDataHashBrute():
+	print(splitter())
+	passwordBase = input("| Enter password base name: ")
+	hash4Brute = input("| Enter hash for brute: ")
+	hashType = input("| Enter hash type for brute(md5,sha1,sha2,sha256,sha512): ")
+	return passwordBase, hash4Brute, hashType
+
+def inputDataGenDict():
+	print(splitter())
+	dictData = input("| Enter dictionary name: ")
+	wordMaxLen = int(input("| Enter max word lenght: "))
+	wordMinLen = int(input("| Enter min word lenght: "))
+	sizeList = int(input("| Enter wordlist size: "))
+	return dictData, wordMinLen, wordMaxLen, sizeList
+
+def inputDataHashDict():
+	passwordBase = input("| Enter password base name: ")
+	filename = input("| Enter new filename for base: ")
+	hashType = input("| Enter hash type for brute(md5,sha1,sha2,sha256,sha512): ")
+	return passwordBase, filename, hashType
+
+def inputData():
+	dictData = input("| Enter dictionary name: ")
+	passwordBase = input("| Enter password base name: ")
+	filename = input("| Enter new filename for base: ")
+	word = input("| Enter check word: ")
+	hash4Brute = input("| Enter hash for brute: ")
+	hashType = input("| Enter hash type for brute(md5,sha1,sha2,sha256,sha512): ")
+	return dictData, passwordBase, filename, word, hash4Brute, hashType
+
+
+def main():
+	print(logo())
+	print(splitter())
+	chMenu = input("\n| Choose function: \n| 1. Brute\n| 2. Create hash dictionary\n| 3. Create brute dictionary(no hash)\n| 4. Generate random words dictionary\n| 5. Exit\n| >>> ")
+	if chMenu == "1":
+		chBruteSubMenu = input("\n| Choose brute type(1 - no hash|2 - hash): ")
+		if chBruteSubMenu == "1":
+			data = inputDataBrute()
+			print(brute(dataList = openDict(data[0]), data = data[1]))
+		elif chBruteSubMenu == "2":
+			data = inputDataHashBrute()
+			print(bruteHash(dataList = openDict(data[0]), data = data[1], hashType = data[2]))
+	elif chMenu == "2":
+		data = inputDataHashDict()
+		hashData(hashType = data[2],dataList = openDict(data[0]),filename = data[1])
+	elif chMenu == "3":
+		print("nothing")
+	elif chMenu == "4":
+		data = inputDataGenDict()
+		genPassDict(filename = data[0],minSize = data[1],maxSize = data[2], sizeList = data[3])
+	elif chMenu == "5":
+		sys.exit()
+
+
+
+
+#dict4Hash = input("Enter dictionary name: ")
+#filename = input("Enter new name: ")
+
+#word = "супер_сложный_пароль"
+#hashWord = hasher(hashType = "sha256", data = word)
+
+#print("Find hash: " + brute(openDict(hashData(hashType = "sha256",dataList = openDict(dict4Hash), filename = filename)), search = hashWord)+"\nWord: "+ word)
+
+if __name__ == "__main__":
+	sys.exit(main())
---
+++ main.py
@@ -0,0 +1,193 @@
+import sys, subprocess, hashlib, zlib, base64
+
+subprocess.call("cls", shell = True)
+
+def atbashCipher(word,alphabet):
+	res = []
+	result = ""
+	normalA = alphabet
+	reverseA = list(reversed(alphabet))
+
+	for i in range(0,len(word)):
+		for j in range(0,len(normalA)):
+			if word[i] == normalA[j]:
+				res.append(reverseA[j])
+	for p in range(0,len(res)):
+		result += res[p]
+	return result
+
+
+def atbashDecipher(word,alphabet):
+	res = []
+	result = ""
+	normalA = alphabet
+	reverseA = list(reversed(alphabet))
+
+	for i in range(0,len(word)):
+		for j in range(0,len(reverseA)):
+			if word[i] == reverseA[j]:
+				res.append(normalA[j])
+	for p in range(0,len(res)):
+		result += res[p]
+	return result
+
+
+def caesarCipher(word,key,alphabet):
+	res = []
+	result = ""
+
+	for i in range(0,len(word)):
+		for j in range(0,len(alphabet)):
+			if word[i] == alphabet[j]:
+				res.append(alphabet[j-int(key)])
+	for p in range(0,len(res)):
+		result += res[p]
+	return result
+
+
+def caesarDecipher(word,key,alphabet):
+	res = []
+	result = ""
+
+	for i in range(0,len(word)):
+		for j in range(0,len(alphabet)):
+			if word[i] == alphabet[j]:
+				res.append(alphabet[j+int(key)])
+	for p in range(0,len(res)):
+		result += res[p]
+	return result
+
+def inputData():
+	return input(splitter()+"\n| Choose language(EN,RU): ").lower(),(input("|| Word: ").lower()).replace(" ",""),input("|| Key: ")
+
+def splitter():
+	return "|==========================="
+
+
+def logo():
+	return splitter()+"\n|=========CiPh3R============"
+
+
+def chooseLang(chLang):
+	ruA = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
+	enA = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	while True:
+		if chLang == "ru":
+			lang = ruA
+			break
+		elif chLang == "en":
+			lang = enA
+			break
+		else:
+			chLang = input(splitter()+"\n| Choose EN or RU only: ")
+	return lang
+
+def atbash():
+	chOper = input(splitter()+"\n| Choose operation:\n| 1.Crypt\n| 2.Decrypt\n| 3.Exit\n| >>> ")
+	data = inputData()
+
+	if chOper == "1":
+		res = "| "+atbashCipher(word = data[1], alphabet = chooseLang(data[0]))
+	elif chOper == "2":
+		res = "| "+atbashDecipher(word = data[1], alphabet = chooseLang(data[0]))
+	elif chOper == "3":
+		sys.exit(main())
+	else:
+		chOper = input(splitter()+"\n| Enter number:\n| 1.Crypt\n| 2.Decrypt\n| 3.Exit\n| >>> ")
+	return res
+
+def caesar():
+	chOper = input(splitter()+"\n| Choose operation:\n| 1.Crypt\n| 2.Decrypt\n| 3.Exit\n| >>> ")
+	data = inputData()
+
+	if chOper == "1":
+		res = "| "+caesarCipher(word = data[1], key = data[2], alphabet = chooseLang(data[0]))
+	elif chOper == "2":
+		res = "| "+caesarDecipher(word = data[1], key = data[2], alphabet = chooseLang(data[0]))
+	elif chOper == "3":
+		sys.exit(main())
+	else:
+		chOper = input(splitter()+"\n| Enter number:\n| 1.Crypt\n| 2.Decrypt\n| 3.Exit\n| >>> ")
+	return res, data[2]
+
+def saveToFile(data):
+	with open('results.txt', 'a',encoding = "UTF-8") as file:
+		file.write(data+'\n')
+		file.close
+	return data
+
+
+def hash():
+	chHash = input(splitter()+"\n| Choose hash function:\n| 1. md5\n| 2. sha1\n| 3. sha256\n| 4. sha512\n| 5. raw\n| 6. base64\n| 7. Exit\n| >>> ")
+	dataHash = input(splitter() + "\n| Enter data for hash: ")
+	while True:
+		if chHash == "1":
+			res = str(hashlib.md5(bytes(dataHash, encoding = "UTF-8")).hexdigest())
+			title = "md5 hash: "
+			break
+		elif chHash == "2":
+			res = str(hashlib.sha1(bytes(dataHash, encoding = "UTF-8")).hexdigest())
+			title = "sha1 hash: "
+			break
+		elif chHash == "3":
+			res = str(hashlib.sha256(bytes(dataHash, encoding = "UTF-8")).hexdigest())
+			title = "sha256 hash: "
+			break
+		elif chHash == "4":
+			res = str(hashlib.sha512(bytes(dataHash, encoding = "UTF-8")).hexdigest())
+			title = "sha512 hash: "
+			break
+		elif chHash == "5":
+			res = str(bytes(dataHash, encoding = "UTF-8"))
+			title = "raw data: "
+			break
+		elif chHash == "6":
+			print("not worked")
+#			res = str(base64.b64encode(bytes(dataHash, encoding = "UTF-8")))
+#			title = "base64 data: "
+		elif chHash =="7":
+			sys.exit(main())
+			break
+		else:
+			chHash = input(splitter()+"\n| Enter number:\n| 1. md5\n| 2. sha1\n| 3. sha256\n| 4. sha512\n| 5. raw\n| 6. Exit\n| >>> ")
+
+	return title + res
+
+
+def cipher():
+	chCipher = input(splitter()+"\n| Choose cipher function:\n| 1. Caesar\n| 2. Atbash\n| 3. Exit\n| >>> ")
+	if chCipher == "1":
+		data = caesar()
+		res = data[0]
+		title  = "caesar's cipher with key("+data[1]+"): "
+	if chCipher == "2":
+		res = atbash()
+		title = "atbash's cipher: "
+	return title + res
+
+
+def mainMenu():
+	chMain = input(splitter()+"\n| Choose function: \n| 1.Hash\n| 2.Cipher\n| 3.Start dictCombine\n| 4.Exit\n| >>> ")
+	while True:
+		if chMain == "1":
+			print("| "+saveToFile(hash()))
+			break
+		elif chMain == "2":
+			print("| "+saveToFile(cipher()))
+			break
+		elif chMain == "3":
+			subprocess.call(["python","dictCombine.py"])
+			subprocess.call(["python3","dictCombine.py"])
+			break
+		elif chMain == "4":
+			sys.exit()
+		else:
+			chMain = input(splitter()+"\n| Enter number: \n| 1.Hash\n| 2.Cipher\n| 3.Exit\n| >>> ")
+
+def main():
+	print(logo())
+	while True:
+		mainMenu()
+
+if __name__ == "__main__":
+	sys.exit(main())
